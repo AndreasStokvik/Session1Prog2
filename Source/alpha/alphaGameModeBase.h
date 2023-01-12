3d545d5966d68e2c3a1c54339a11b4d10600b614
AndreasStokvik <111863574+AndreasStokvik@users.noreply.github.com>
2023-01-12 14:53:02 +0100

    initial commit

---
+++ Source/alpha/alphaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "alphaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALPHA_API AalphaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
